d625c30c63a743589302476e7f9f1eb1326f2b9a
Hamad Anwar <rh676838@gmail.com>
2023-08-08 23:13:29 +0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,17 @@ import 'package:weather/view/splash/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen()
+      home: const SplashScreen()
     );
   }
 }
